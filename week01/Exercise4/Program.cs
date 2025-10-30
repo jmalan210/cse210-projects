a4de433387ee40a2a5a7d6ad20fd40cb99d9c6bc
jmalan210 <>
2025-10-30 15:40:09 -0600

    Completed Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            else
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = numbers.Sum();
+        int length = numbers.Count;
+        float avg = ((float)sum) / length;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+
+        int largestNumber = int.MinValue;
+        foreach (int item in numbers)
+        {
+            if (item > largestNumber)
+            {
+                largestNumber = item;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
+        int smallestPositive = int.MaxValue;
+        foreach (int item in numbers)
+        {
+            if (item > 0 && item < smallestPositive)
+            {
+                smallestPositive = item;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        
     }
+
 }

3961f9c6c768d47ef81e46fd30b85e1e63c28d72
jmalan210 <>
2025-10-30 11:52:24 -0600

    completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string response = "yes";
+
+
+        while (response == "yes")
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 1;
+            while (magicNumber != guess)
+            {
+                Console.Write("What is your guess (1-100, whole numbers?) ");
+
+                guess = int.Parse(Console.ReadLine());
+
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses!");
+                }
+
+                guessCount++;
+
+
+            }
+            Console.Write("Do you want to play again? (yes/no) ");
+            response = Console.ReadLine();
+        }
+       
     }
+   
 }

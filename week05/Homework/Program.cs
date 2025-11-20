bab730ad44f771236bbf84f727e8cd018cd83750
jmalan210 <>
2025-11-20 12:36:12 -0700

    Week 5 Inheritance  Activity Complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathassignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathassignment1.GetSummary());
+        Console.WriteLine(mathassignment1.GetHomeworkList());
+
+        WritingAssignment writingassignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingassignment1.GetSummary());
+        Console.WriteLine(writingassignment1.GetWritingInformation());
+
+
     }
 }

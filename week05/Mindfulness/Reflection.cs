a8065fc27a68d763887baead03116197767830e3
jmalan210 <>
2025-11-20 20:32:28 -0700

    breathing exercise works

--- week05/Mindfulness/Reflection.cs
+++ week05/Mindfulness/Reflection.cs
@@ -1,5 +1,22 @@
 public class Reflection : Activity
 {
+
+    private List<string> _prompts = new List<string>() {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."};
+    private List<string> _questions = new List<string>() {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "How can you keep this experience in mind in the future?"
+    };
+
     public Reflection()
         : base("This activity will help you reflect on times in your life when you have shown strength and resilience.This will help you recognize the power you have and how you can use it in other aspects of your life.", "Well done on completing your reflection exercise!", "reflection")
     {
